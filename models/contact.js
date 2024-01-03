aae565228fc0645a31ee068d9a8aa642ffdfa53d
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-01-03 22:09:57 +0100

    task OK

--- models/contact.js
+++ models/contact.js
@@ -7,8 +7,8 @@ const mainSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string()
     .email({
-      minDomainSegments: 2,
-      tlds: { allow: ["com", "net"] },
+      minDomainSegments: 3,
+      tlds: { allow: ["com", "net", "ua"] },
     })
     .required(),
   phone: Joi.string(),
